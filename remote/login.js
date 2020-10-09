cbbd800f22875e74bb0ed7d1c07984b4d5e63345
Ryan <ryan@ryanwans.com>
2020-10-08 21:54:00 -0400

    Automated Push

--- remote/login.js
+++ remote/login.js
@@ -1,7 +1,9 @@
 exports.Attempt = async (FormData) => {
-    import fetch from 'electron-fetch';
+    const fetch = require('electron-fetch').default
 
     var endpoint = "https://caplet.ryanwans.com/a3/l/q/a/l?form=js&stamp="+Date.now();
+
+    var WINDOW = new Object();
 
     var toSend = {
         data: FormData
@@ -9,19 +11,28 @@ exports.Attempt = async (FormData) => {
 
     let QueryPromise = new Promise((resolve, reject) => {
         resolve((() => {
-            return fetch(endpoint, { 
+            let f;
+            fetch(endpoint, { 
                     method: 'POST',
                     body:    toSend,
                     headers: { 'Content-Type': 'application/json' },
                 })
                 .then(res => res.json())
-                .then(json => {return json;});
+                .then(json => f=json);
+            console.log("Fetch Request Completed.");
+            return f;
         })());
     });
 
     let AuthTrue = await QueryPromise;
+
+    if("undefined" == typeof AuthTrue) {
+        AuthTrue = new Object();
+        AuthTrue.auth = 0;
+    }
     if("object" != typeof AuthTrue) {
         AuthTrue = JSON.parse(AuthTrue);
     }
+
     return AuthTrue.auth;
 }
